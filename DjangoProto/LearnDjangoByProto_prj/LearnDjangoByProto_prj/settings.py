26807410c5407cc462efdaa0ce19f6fe030ec754
CCDM <catandbri2002@aol.com>
2020-09-09 17:54:49 -0400

    chapter 11 homework done
    Change-Id: I197a6fb2876707c6bc79249bb6cf513f72dda02b

--- DjangoProto/LearnDjangoByProto_prj/LearnDjangoByProto_prj/settings.py
+++ DjangoProto/LearnDjangoByProto_prj/LearnDjangoByProto_prj/settings.py
@@ -49,14 +49,14 @@ LOGIN_URL = 'auth_login'
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Rango',
-    'registration',
+    'registration',  # should be immediately above contrib.admin
+    'django.contrib.admin',
 ]
 
 MIDDLEWARE = [
