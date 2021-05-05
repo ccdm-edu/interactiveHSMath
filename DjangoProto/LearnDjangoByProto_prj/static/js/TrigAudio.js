a9eb400d87d0d029c5fad6de3f84f060cddd9256
ccdm-edu <cathy.demeyer@yahoo.com>
2021-05-05 16:43:32 -0400

    cleaned up javascript in tones stuff, working on graph overlap
    Change-Id: I1df2267e37162d4b9ee0fa7a8eab00ff431144b4

--- DjangoProto/LearnDjangoByProto_prj/static/js/TrigAudio.js
+++ DjangoProto/LearnDjangoByProto_prj/static/js/TrigAudio.js
@@ -1,102 +1,95 @@
 // NOTE, this function uses WebAudio in library Tone.js.  At this time, it does not work in IE Edge
-var currFreq = document.getElementById("in-range-freq");
-var currAmp = document.getElementById("in-range-amp");
-var currPhase = document.getElementById("in-range-phase")
-
-
-// effectively, this is a singleton, a static class
-class ToneHandler {
-	
-	static ToneIsOnNow = false;
-	static osc = new Tone.Oscillator(); 
-
-
-	
-	constructor() {
-		// initialize the value boxes for each element
-		// using innerHTML allows use of XSS hack since the input would be interpreted as HTML and executed.  Here, we just
-		// take the text only and put in label box
-		document.getElementById("currFreq").textContent = document.getElementById("in-range-freq").value;
-		document.getElementById("currAmp").textContent = document.getElementById("in-range-amp").value;
-		document.getElementById("currPhase").textContent = document.getElementById("in-range-phase").value;
-		
+function changeFreq()
+{
+	//min and max freq chosen depends on audio speakers used, my speakers can just barely respond at 100 Hz
+	currFreq= document.getElementById("in-range-freq")
+	document.getElementById("currFreq").textContent = currFreq.value;
+	if (ToneIsOnNow==true) {
+		osc.frequency.value = currFreq.value;
+		// if tone isn't on, don't have to change anything...
 	}
-	
-	static ChangeFreq()
-	{
-		//min and max freq chosen depends on audio speakers used, my speakers can just barely respond at 100 Hz
-		currFreq= document.getElementById("in-range-freq")
-		document.getElementById("currFreq").textContent = currFreq.value;
-		if (this.ToneIsOnNow==true) {
-			this.osc.frequency.value = currFreq.value;
-			// if tone isn't on, don't have to change anything...
-		}
-	}
-	static ChangeAmp()
-	{
-		currAmp = document.getElementById("in-range-amp")
-		document.getElementById("currAmp").textContent = currAmp.value;
-		if (this.ToneIsOnNow==true) {
-			var tonejs_dB = -40 + 20.0 * Math.log10(currAmp.value);
-			this.osc.volume.value = tonejs_dB;
-			// if tone isn't on, don't have to change anything...
-		}
-	}
-	static ChangePhase()
-	{
-		currPhase = document.getElementById("in-range-phase")
-		document.getElementById("currPhase").textContent = currPhase.value;
-		if (this.ToneIsOnNow==true) {
-			this.osc.phase = currPhase.value;
-			console.log("current phase is " + currPhase.value)
-			// if tone isn't on, don't have to change anything...
-		}
-	}
-	
-	static StartStopTone()
-	{	
-		if (typeof this.ToneIsOnNow == "undefined")  {
-			// First time in, 
-			this.ToneIsOnNow = false;
-		};
-		// convert amplitude to what tone.js calls decibels.  In tone.js, -40 dB is very quiet
-		// and 0 dB is plenty loud enough.  I know this isn't the music industry definition (decibel SPL where 0 dB
-		// is the quietest sound one can hear and 100 dB will cause hearing damage) so I will say Amplitude = 1
-		// is min audible and amplitude 40 dB higher (40 = 20log(A1/A0) or A1=100 if A0 = 1) is max we want to put out
+};
+function changeAmp()
+{
+	currAmp = document.getElementById("in-range-amp")
+	document.getElementById("currAmp").textContent = currAmp.value;
+	if (ToneIsOnNow==true) {
 		var tonejs_dB = -40 + 20.0 * Math.log10(currAmp.value);
-		if (this.ToneIsOnNow==false) {
-			// currently false, clicked by user and about to be true 
-			this.osc = new Tone.Oscillator({
-					frequency: currFreq.value, 
-					volume: tonejs_dB,
-					phase: currPhase.value,
-					type:"sine"});
-			this.osc.toDestination().start();	
-			document.getElementById("start-stop-button").value = "Stop Tone";
-			this.ToneIsOnNow = true;
-		} else {
-			this.osc.toDestination().stop();
-			document.getElementById("start-stop-button").value = "Start Tone";
-			this.ToneIsOnNow = false;
-		}
-		
+		osc.volume.value = tonejs_dB;
+		// if tone isn't on, don't have to change anything...
+	}
+};
+function changePhase()
+{
+	currPhase = document.getElementById("in-range-phase")
+	document.getElementById("currPhase").textContent = currPhase.value;
+	if (ToneIsOnNow==true) {
+		osc.phase = currPhase.value;
+		// if tone isn't on, don't have to change anything...
+	}
+};
+function startStopTone()
+{	
+	if (typeof ToneIsOnNow == "undefined")  {
+		// First time in, 
+		ToneIsOnNow = false;
+	};
+	// convert amplitude to what tone.js calls decibels.  In tone.js, -40 dB is very quiet
+	// and 0 dB is plenty loud enough.  I know this isn't the music industry definition (decibel SPL where 0 dB
+	// is the quietest sound one can hear and 100 dB will cause hearing damage) so I will say Amplitude = 1
+	// is min audible and amplitude 40 dB higher (40 = 20log(A1/A0) or A1=100 if A0 = 1) is max we want to put out
+	var tonejs_dB = -40 + 20.0 * Math.log10(currAmp.value);
+	if (ToneIsOnNow==false) {
+		// currently false, clicked by user and about to be true 
+		osc = new Tone.Oscillator({
+				frequency: currFreq.value, 
+				volume: tonejs_dB,
+				phase: currPhase.value,
+				type:"sine"});
+		osc.toDestination().start();	
+		document.getElementById("start-stop-button").value = "Stop Tone";
+		ToneIsOnNow = true;
+	} else {
+		osc.toDestination().stop();
+		document.getElementById("start-stop-button").value = "Start Tone";
+		ToneIsOnNow = false;
 	}
 	
+};
+function drawTone()
+{
 
- 
-}
+	fillInArrays();
+	// CHART js hint:  update time, need to add the new and THEN remove the old.  X axis doesn't like to be empty...
+	
+    // update tone, remove old (although for now, just one data set), add new
+    sine_plot_100_1k.data.datasets.forEach((dataset) => {
+        dataset.data.pop();
+    });
+    sine_plot_100_1k.data.datasets.forEach((dataset) => {
+        dataset.data.push(ampLo);
+    });	                
+    //sine_plot.options.scales.xAxes[0].ticks.minRotation = 45;
+    sine_plot_100_1k.update();
+    
+    // update tone, remove old (although for now, just one data set), add new
+    sine_plot_1k_10k.data.datasets.forEach((dataset) => {
+        dataset.data.pop();
+    });
+    sine_plot_1k_10k.data.datasets.forEach((dataset) => {
+        dataset.data.push(ampHi);
+    });	                
+    //sine_plot.options.scales.xAxes[0].ticks.minRotation = 45;
+    sine_plot_1k_10k.update();
+    
+};
 //want to round but leave value as number, not string, so toFixed() is out. 
 //If x axis is a number, chart js will figure out grid lines/step size as needed
 function roundFP(number, prec) {
     var tempnumber = number * Math.pow(10, prec);
     tempnumber = Math.round(tempnumber);
     return tempnumber / Math.pow(10, prec);
-}
-// yeah, shouldn't just update global variables but need to tackle issue of namespaces
-var timeMsLo = [];
-var ampLo = [];
-var timeMsHi = [];
-var ampHi = [];
+};
 function fillInArrays(){
 	const NUM_PTS_PLOT = 200;
 	const NUM_PTS_PLOT_LO = 1000;
@@ -114,11 +107,25 @@ function fillInArrays(){
 		ampHi[i] = currAmp.value * Math.sin(2 * Math.PI * (currFreq.value * i * samplePeriodHi + currPhase.value / 360.0) );
 		timeMsHi[i] = roundFP(i * samplePeriodHi * 1000, 2);
 	}	
-}
+};
 
-var ctxLo = document.getElementById('sine_plotsLo').getContext("2d");;
-var ctxHi = document.getElementById('sine_plotsHi').getContext("2d");;
-fillInArrays();
+
+var currFreq = document.getElementById("in-range-freq");
+var currAmp = document.getElementById("in-range-amp");
+var currPhase = document.getElementById("in-range-phase")
+
+var ToneIsOnNow = false;
+var osc = new Tone.Oscillator(); 
+
+// yeah, shouldn't just update global variables but need to tackle issue of namespaces
+var timeMsLo = [];
+var ampLo = [];
+var timeMsHi = [];
+var ampHi = [];
+
+var ctxLo = document.getElementById('sine_plotsLo').getContext("2d");
+var ctxHi = document.getElementById('sine_plotsHi').getContext("2d");
+
 //if x and y axis labels don't show, probably chart size isn't big enough and they get clipped out
 const CHART_OPTIONS = {
 	maintainAspectRatio: false,  //uses the size it is given
@@ -164,6 +171,21 @@ var sine_plot_100_1k = new Chart(ctxLo, {
     options: TOP_CHART
  }
 );
+// if x and y axis labels don't show, probably chart size isn't big enough and they get clipped out
+var sine_plot_1k_10k = new Chart(ctxHi, {
+    type: 'line',
+    data: {
+    	labels: timeMsHi,
+        datasets: [{
+            label: 'Tone Graph',
+            data: ampHi,
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            }]
+    },
+    options: CHART_OPTIONS
+ }
+);
 
 
 // draw line between the charts----------------------------------------------------------------------------------
@@ -178,7 +200,7 @@ ctxExpandTime.lineWidth = 2;
 
 // draw a line from 0 ms to 0 ms
 ctxExpandTime.beginPath();
-const ZERO = 50;  // about where the zero axis ends up on the canvas
+const ZERO = 55;  // about where the zero axis ends up on the canvas
 ctxExpandTime.moveTo(ZERO, 0);
 ctxExpandTime.lineTo(ZERO, CNV_H);
 // left arrow
@@ -208,51 +230,24 @@ ctxExpandTime.fillText("1ms", 80, CNV_H/2);
 ctxExpandTime.fillText("expanded", 80, CNV_H/2 + 20);
 //--------------------------------------------------------------------------------------------------------------------
 
-// if x and y axis labels don't show, probably chart size isn't big enough and they get clipped out
-var sine_plot_1k_10k = new Chart(ctxHi, {
-    type: 'line',
-    data: {
-    	labels: timeMsHi,
-        datasets: [{
-            label: 'Tone Graph',
-            data: ampHi,
-            fill: false,
-            borderColor: 'rgb(75, 192, 192)',
-            }]
-    },
-    options: CHART_OPTIONS
- }
-);
 
-function drawTone()
-{
-	fillInArrays();
-	// CHART js hint:  update time, need to add the new and THEN remove the old.  X axis doesn't like to be empty...
-	
-    // update tone, remove old (although for now, just one data set), add new
-    sine_plot_100_1k.data.datasets.forEach((dataset) => {
-        dataset.data.pop();
-    });
-    sine_plot_100_1k.data.datasets.forEach((dataset) => {
-        dataset.data.push(ampLo);
-    });	                
-    //sine_plot.options.scales.xAxes[0].ticks.minRotation = 45;
-    sine_plot_100_1k.update();
-    
-    // update tone, remove old (although for now, just one data set), add new
-    sine_plot_1k_10k.data.datasets.forEach((dataset) => {
-        dataset.data.pop();
-    });
-    sine_plot_1k_10k.data.datasets.forEach((dataset) => {
-        dataset.data.push(ampHi);
-    });	                
-    //sine_plot.options.scales.xAxes[0].ticks.minRotation = 45;
-    sine_plot_1k_10k.update();
-    
-}
+//initialize default values for tone
+fillInArrays();
+drawTone();
+document.getElementById("currFreq").textContent = document.getElementById("in-range-freq").value;
+document.getElementById("currAmp").textContent = document.getElementById("in-range-amp").value;
+document.getElementById("currPhase").textContent = document.getElementById("in-range-phase").value;
 
-//purpose is to fill in labels for initial settings, not used thereafter
-var myTone = new ToneHandler;
+
+// set up observers who react to user change
 var toneChangeUpdt = document.getElementById("toneChanges");
 toneChangeUpdt.addEventListener('input', drawTone, false);
+var freqUpdt = document.getElementById("in-range-freq");
+freqUpdt.addEventListener('input', changeFreq, false);
+var ampUpdt = document.getElementById("in-range-amp");
+ampUpdt.addEventListener('input', changeAmp, false);
+var phaseUpdt = document.getElementById("in-range-phase");
+phaseUpdt.addEventListener('input', changePhase, false);
+var onOffUpdt = document.getElementById("start-stop-button");
+onOffUpdt.addEventListener('click', startStopTone, false);
 
