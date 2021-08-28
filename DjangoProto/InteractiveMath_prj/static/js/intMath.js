2d22271b0a1d43d2ade40ce0daeeef1f4ecf7a4a
ccdm-edu <cathy.demeyer@yahoo.com>
2021-08-28 16:55:23 -0400

    working on home page animations
    Change-Id: I6a25b8522eca62730faa61a507e2edb6679957b8

---
+++ DjangoProto/InteractiveMath_prj/static/js/intMath.js
@@ -0,0 +1,6 @@
+'use strict'
+//JQuery, dont do this script until document DOM objects are loaded and ready
+$(function () {
+	// tooltips are disabled by default, need to enable them all for bootstrap styling
+    $('[data-toggle="tooltip"]').tooltip()
+})
