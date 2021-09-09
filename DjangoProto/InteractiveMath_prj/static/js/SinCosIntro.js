d461622db39d18b57c78060023fd4aa0159178a7
ccdm-edu <cathy.demeyer@yahoo.com>
2021-09-09 18:58:02 -0400

    got sine graph animation working, mostly
    Change-Id: Ib0fc8afe4234676a727447baf7c00b5da581112f

--- DjangoProto/InteractiveMath_prj/static/js/SinCosIntro.js
+++ DjangoProto/InteractiveMath_prj/static/js/SinCosIntro.js
@@ -124,7 +124,7 @@ $(function() {
 			x: CIRC_X0,
 			y: CIRC_Y0 + Math.round((amp * CIRC_RAD)),
 			xcos: 0,
-			ysin: Math.round((amp * CIRC_RAD)),
+			ysin: -Math.round((amp * CIRC_RAD)),
 			angleRad: Math.PI/2,
 		},
 		{
@@ -177,7 +177,7 @@ $(function() {
 	
 	schoolAngles.forEach(dot => {
 		  ctxExpandableUnitCircle.beginPath();
-		  // create dots for user clickable dots
+		  // create dots for user clickable sine/cos/angle graphics
 		  ctxExpandableUnitCircle.arc(dot.x, dot.y, DOT_RADIUS, 0, 2 * Math.PI, true);
 		  ctxExpandableUnitCircle.fillStyle = 'yellow';
 		  ctxExpandableUnitCircle.fill();
@@ -185,6 +185,8 @@ $(function() {
 		  ctxExpandableUnitCircle.closePath();
 		  
 	});
+	//*****
+	// used to draw arrows on lines
 	function leftArrow(x, y) {
 		ctxExpandableUnitCircle.beginPath();
 		ctxExpandableUnitCircle.moveTo(x, y);
@@ -221,7 +223,7 @@ $(function() {
 		ctxExpandableUnitCircle.stroke();
 		ctxExpandableUnitCircle.closePath();
 	};	
-	
+	// *****
 	// make arrows for XY axis 
 	leftArrow(CIRC_X0 - HALF_AXIS, CIRC_Y0);
 	rightArrow(CIRC_X0 + HALF_AXIS, CIRC_Y0);
@@ -232,36 +234,37 @@ $(function() {
 	// NOW draw the two sin/cos axis off to the right of circle
 	//****
 
-	const SIN_Y_CENTER = 175;
-	const TRIG_X_CENTER = 550;
-	const COS_Y_CENTER = 480;
-	const TRIG_AXIS = 350; 
+	const SIN_Y_ORIGIN = 175;
+	const TRIG_X_ORIGIN = 500;
+	const COS_Y_ORIGIN = 480;
+	const TRIG_AXIS = 420; 
 	const MAX_AMP_AXIS = MAX_AMP * CIRC_RAD + 10;  // needs to be at least MAX_AMP*CIRC_RAD to match the circle values
 	// draw sin axis off to right and a little above
 	// draw x axis
 	ctxExpandableUnitCircle.beginPath();
-	ctxExpandableUnitCircle.moveTo(TRIG_X_CENTER - TRIG_AXIS/8, SIN_Y_CENTER);
-	ctxExpandableUnitCircle.lineTo(TRIG_X_CENTER + TRIG_AXIS, SIN_Y_CENTER);
+	ctxExpandableUnitCircle.moveTo(TRIG_X_ORIGIN - TRIG_AXIS/8, SIN_Y_ORIGIN);
+	ctxExpandableUnitCircle.lineTo(TRIG_X_ORIGIN + TRIG_AXIS, SIN_Y_ORIGIN);
 	ctxExpandableUnitCircle.fillStyle = 'black';
 	ctxExpandableUnitCircle.font = '20px Arial';
-	ctxExpandableUnitCircle.fillText("\u03b8", TRIG_X_CENTER + TRIG_AXIS, SIN_Y_CENTER);	
+	ctxExpandableUnitCircle.fillText("\u03b8", TRIG_X_ORIGIN + TRIG_AXIS, SIN_Y_ORIGIN);	
 	// draw y axis
-	ctxExpandableUnitCircle.moveTo(TRIG_X_CENTER, SIN_Y_CENTER + MAX_AMP_AXIS);
-	ctxExpandableUnitCircle.lineTo(TRIG_X_CENTER, SIN_Y_CENTER - MAX_AMP_AXIS);
+	ctxExpandableUnitCircle.moveTo(TRIG_X_ORIGIN, SIN_Y_ORIGIN + MAX_AMP_AXIS);
+	ctxExpandableUnitCircle.lineTo(TRIG_X_ORIGIN, SIN_Y_ORIGIN - MAX_AMP_AXIS);
 	ctxExpandableUnitCircle.fillStyle = 'black';
 	ctxExpandableUnitCircle.font = '20px Arial';
-	ctxExpandableUnitCircle.fillText("Rsin\u03b8", TRIG_X_CENTER, SIN_Y_CENTER - MAX_AMP_AXIS);
+	ctxExpandableUnitCircle.fillText("Rsin\u03b8", TRIG_X_ORIGIN, SIN_Y_ORIGIN - MAX_AMP_AXIS);
 	ctxExpandableUnitCircle.stroke();
 	ctxExpandableUnitCircle.closePath();
 	// make arrows for Angle-Sin graph
-	leftArrow(TRIG_X_CENTER - TRIG_AXIS/8, SIN_Y_CENTER);
-	rightArrow(TRIG_X_CENTER + TRIG_AXIS, SIN_Y_CENTER);
-	upArrow(TRIG_X_CENTER, SIN_Y_CENTER - MAX_AMP_AXIS);
-	downArrow(TRIG_X_CENTER, SIN_Y_CENTER + MAX_AMP_AXIS);  
+	leftArrow(TRIG_X_ORIGIN - TRIG_AXIS/8, SIN_Y_ORIGIN);
+	rightArrow(TRIG_X_ORIGIN + TRIG_AXIS, SIN_Y_ORIGIN);
+	upArrow(TRIG_X_ORIGIN, SIN_Y_ORIGIN - MAX_AMP_AXIS);
+	downArrow(TRIG_X_ORIGIN, SIN_Y_ORIGIN + MAX_AMP_AXIS);  
 	
 	// draw dashed amplitude bar at current point
 	ctxExpandableUnitCircle.beginPath();
 	ctxExpandableUnitCircle.strokeStyle = "blue";
+	// full theta pix is the number of pixels between 0 and 2pi for both graphs
 	const FULL_THETA_PIX = TRIG_AXIS - 50;
 	const DASH_SEG_PIX = 3;
 	function calcSine(j) {
@@ -271,36 +274,36 @@ $(function() {
     	var yLo = calcSine(i); 
     	var yHi = calcSine(i+DASH_SEG_PIX);
     	// remember, on bitmap, y increases as you go from top to bottom
-  		ctxExpandableUnitCircle.moveTo(TRIG_X_CENTER + i, SIN_Y_CENTER - yLo); 
+  		ctxExpandableUnitCircle.moveTo(TRIG_X_ORIGIN + i, SIN_Y_ORIGIN - yLo); 
   		// make a dash seg and then skip and redo again.  
-    	ctxExpandableUnitCircle.lineTo(TRIG_X_CENTER + i + DASH_SEG_PIX, SIN_Y_CENTER - yHi); 
+    	ctxExpandableUnitCircle.lineTo(TRIG_X_ORIGIN + i + DASH_SEG_PIX, SIN_Y_ORIGIN - yHi); 
   	}
 	ctxExpandableUnitCircle.stroke(); 
 
 	// draw cos axis off to right and below
 	// draw x axis
 	ctxExpandableUnitCircle.beginPath();
-	ctxExpandableUnitCircle.moveTo(TRIG_X_CENTER - TRIG_AXIS/8, COS_Y_CENTER);
-	ctxExpandableUnitCircle.lineTo(TRIG_X_CENTER + TRIG_AXIS, COS_Y_CENTER);
+	ctxExpandableUnitCircle.moveTo(TRIG_X_ORIGIN - TRIG_AXIS/8, COS_Y_ORIGIN);
+	ctxExpandableUnitCircle.lineTo(TRIG_X_ORIGIN + TRIG_AXIS, COS_Y_ORIGIN);
 	ctxExpandableUnitCircle.fillStyle = 'black';
 	ctxExpandableUnitCircle.strokeStyle = "black";
 	ctxExpandableUnitCircle.font = '20px Arial';
-	ctxExpandableUnitCircle.fillText("\u03b8", TRIG_X_CENTER + TRIG_AXIS, COS_Y_CENTER);	
+	ctxExpandableUnitCircle.fillText("\u03b8", TRIG_X_ORIGIN + TRIG_AXIS, COS_Y_ORIGIN);	
 	// draw y axis
-	ctxExpandableUnitCircle.moveTo(TRIG_X_CENTER, COS_Y_CENTER + MAX_AMP_AXIS);
-	ctxExpandableUnitCircle.lineTo(TRIG_X_CENTER, COS_Y_CENTER - MAX_AMP_AXIS);
+	ctxExpandableUnitCircle.moveTo(TRIG_X_ORIGIN, COS_Y_ORIGIN + MAX_AMP_AXIS);
+	ctxExpandableUnitCircle.lineTo(TRIG_X_ORIGIN, COS_Y_ORIGIN - MAX_AMP_AXIS);
 	ctxExpandableUnitCircle.fillStyle = 'black';
 	ctxExpandableUnitCircle.font = '20px Arial';
-	ctxExpandableUnitCircle.fillText("Rcos\u03b8", TRIG_X_CENTER, COS_Y_CENTER - MAX_AMP_AXIS);	
+	ctxExpandableUnitCircle.fillText("Rcos\u03b8", TRIG_X_ORIGIN, COS_Y_ORIGIN - MAX_AMP_AXIS);	
 	
 	ctxExpandableUnitCircle.stroke();
 	ctxExpandableUnitCircle.closePath();	
 	
 	// make arrows for Angle-Cos graph
-	leftArrow(TRIG_X_CENTER - TRIG_AXIS/8, COS_Y_CENTER);
-	rightArrow(TRIG_X_CENTER + TRIG_AXIS, COS_Y_CENTER);
-	upArrow(TRIG_X_CENTER, COS_Y_CENTER - MAX_AMP_AXIS);
-	downArrow(TRIG_X_CENTER, COS_Y_CENTER + MAX_AMP_AXIS); 
+	leftArrow(TRIG_X_ORIGIN - TRIG_AXIS/8, COS_Y_ORIGIN);
+	rightArrow(TRIG_X_ORIGIN + TRIG_AXIS, COS_Y_ORIGIN);
+	upArrow(TRIG_X_ORIGIN, COS_Y_ORIGIN - MAX_AMP_AXIS);
+	downArrow(TRIG_X_ORIGIN, COS_Y_ORIGIN + MAX_AMP_AXIS); 
 	
 	// draw dashed amplitude bar at current point
 	ctxExpandableUnitCircle.beginPath();
@@ -312,11 +315,68 @@ $(function() {
     	var yLo = calcCos(i); 
     	var yHi = calcCos(i+DASH_SEG_PIX);
     	// remember, on bitmap, y increases as you go from top to bottom
-  		ctxExpandableUnitCircle.moveTo(TRIG_X_CENTER + i, COS_Y_CENTER - yLo); 
+  		ctxExpandableUnitCircle.moveTo(TRIG_X_ORIGIN + i, COS_Y_ORIGIN - yLo); 
   		// make a dash seg and then skip and redo again.  
-    	ctxExpandableUnitCircle.lineTo(TRIG_X_CENTER + i + DASH_SEG_PIX, COS_Y_CENTER - yHi); 
+    	ctxExpandableUnitCircle.lineTo(TRIG_X_ORIGIN + i + DASH_SEG_PIX, COS_Y_ORIGIN - yHi); 
   	}
 	ctxExpandableUnitCircle.stroke(); 
+	
+	function vertical_fract(x, y, num, denom) {
+		ctxExpandableUnitCircle.font = '12px Arial';
+		ctxExpandableUnitCircle.setLineDash([]);  // no dash
+		// do numerator
+		ctxExpandableUnitCircle.fillText(num, x,y);
+		// do dividing line
+		if (denom != "") {
+			// means we actually have a denominator
+			ctxExpandableUnitCircle.beginPath();
+			ctxExpandableUnitCircle.strokeStyle = "black";
+			ctxExpandableUnitCircle.moveTo(x, y + 3);
+			ctxExpandableUnitCircle.lineTo(x + 10, y+3);
+			ctxExpandableUnitCircle.stroke();
+		}
+		// do denominator
+		ctxExpandableUnitCircle.fillText(denom, x, y+16);	
+	}
+	
+	//put ticks on the sin/cos axis.  FULL_THETA_PIX is the number of pixels in 0 to 2pi (full scale)
+	// thetaGraph is indexed by the yellow dots on the circle (16 in total) and represents the angle on a sine/cos graph
+	// in pixels from the origin
+	let thetaGraph = [];
+	let pi = "\u03c0"
+	thetaGraph[0] = {pix: 0, num: "", den: ""};
+	thetaGraph[1] = {pix: FULL_THETA_PIX/12, num:pi, den: "6"};  // pi/6 angle
+	thetaGraph[2] = {pix: FULL_THETA_PIX/8, num:pi, den: "4"};   // pi/4
+	thetaGraph[3] = {pix: FULL_THETA_PIX/6, num:pi, den: "3"};  // pi/3
+	thetaGraph[4] = {pix: FULL_THETA_PIX/4, num: pi, den: "2"};  // pi/2
+	thetaGraph[5] = {pix: FULL_THETA_PIX/4 + FULL_THETA_PIX/12, num: "2" + pi, den: "3"};  // pi/2 + pi/6
+	thetaGraph[6] = {pix: FULL_THETA_PIX/4 + FULL_THETA_PIX/8, num: "3" + pi, den: "4"};  // pi/2 + pi/4
+	thetaGraph[7] = {pix: FULL_THETA_PIX/4 + FULL_THETA_PIX/6, num: "5" + pi, den: "6"};  // pi/2 + pi/3
+	thetaGraph[8] = {pix: FULL_THETA_PIX/2, num: pi, den: ""};  // pi
+	thetaGraph[9] = {pix: FULL_THETA_PIX/2 + FULL_THETA_PIX/12, num: "7" + pi, den: "6"};  // pi + pi/6
+	thetaGraph[10] = {pix: FULL_THETA_PIX/2 + FULL_THETA_PIX/8, num: "5" + pi, den: "4"};  // pi + pi/4
+	thetaGraph[11] = {pix: FULL_THETA_PIX/2 + FULL_THETA_PIX/6, num: "4" + pi, den: "3"};  // pi + pi/3
+	thetaGraph[12] = {pix: 3*FULL_THETA_PIX/4, num: "3" + pi, den: "2"};  // 3pi/2 
+	thetaGraph[13] = {pix: 3*FULL_THETA_PIX/4 + FULL_THETA_PIX/12, num: "5" + pi, den: "3"};  // 3pi/2 + pi/6 = 5pi/3
+	thetaGraph[14] = {pix: 3*FULL_THETA_PIX/4 + FULL_THETA_PIX/8, num: "7" + pi, den: "4"};  //  3pi/2 + pi/4 = 7pi/4
+	thetaGraph[15] = {pix: 3*FULL_THETA_PIX/4 + FULL_THETA_PIX/6, num: "11" + pi, den: "6"};  //  3pi/2 + pi/3 = 11pi/6
+	thetaGraph[16] = {pix: FULL_THETA_PIX, num: "2" + pi, den: ""};  //  2pi
+
+			
+	// add tick marks to sin/cos axes, no need to mark 0 degrees
+	for (var i = 1; i < thetaGraph.length; i++) {
+		ctxExpandableUnitCircle.beginPath();
+		ctxExpandableUnitCircle.setLineDash([5,25]); // dash the lines to mostly space 
+		ctxExpandableUnitCircle.moveTo(TRIG_X_ORIGIN + thetaGraph[i].pix, SIN_Y_ORIGIN);
+		ctxExpandableUnitCircle.lineTo(TRIG_X_ORIGIN + thetaGraph[i].pix, COS_Y_ORIGIN);
+		ctxExpandableUnitCircle.strokeStyle = "green";
+		ctxExpandableUnitCircle.stroke();
+		// want label under the axis, want pi/N written vertically
+		vertical_fract(TRIG_X_ORIGIN + thetaGraph[i].pix, COS_Y_ORIGIN + 12, thetaGraph[i].num, thetaGraph[i].den);
+		vertical_fract(TRIG_X_ORIGIN + thetaGraph[i].pix, SIN_Y_ORIGIN + 12, thetaGraph[i].num, thetaGraph[i].den);
+
+	}
+	
 	
 	let circleDotsCanvas =  $("#AmpSinCosCircle").get(0);
 	// NOW we have the background image done.  As users click on a point and new stuff happens, we always come back to 
@@ -334,10 +394,79 @@ $(function() {
 	
 	
 	
+	
+	const NUM_ANIMATION_SIN_COS = 5;
+	let sinLineMovement = [];
+	let cosLineMovement = [];
+	let line;
+	function animate_circ2graph(whichGraph, x1Axis, y1Axis, x1Circ, y1Circ, indx) {
+		// find the end place for this line to be used in animation using indx, x2Top = x2Bottom since vert lines
+		let x2Axis = TRIG_X_ORIGIN + thetaGraph[indx].pix;
+		let x2Graph = x2Axis;
+		let y2Axis;
+		if (whichGraph ==="sin") {
+			 //we know x axis origin of sin graph is at SIN_Y_ORIGIN
+			y2Axis = SIN_Y_ORIGIN;
+		} else if (whichGraph == "cos") {
+			 //we know x axis origin of cos graph is at COS_Y_ORIGIN
+			y2Axis = COS_Y_ORIGIN;
+		}
+		// remember, Y grows downward (yeah, counterintuitive
+		let y2Graph = y2Axis - schoolAngles[indx].ysin;
+		
+		console.log("i = "+indx+"start is ( " + x1Axis + "," + y1Axis + ") on axis and (" + x1Circ +"," + y1Circ);
+		console.log(" on plot, sine is " + schoolAngles[indx].ysin);
+		console.log("  end at point (" + x2Axis +","+ y2Axis +") on axis and (" + x2Graph+","+ y2Graph);
+		
+		if (whichGraph ==="sin") {
+			// sin line starts out vertical and ends up vertical
+			let deltaX_Pt = (x2Graph - x1Circ)/NUM_ANIMATION_SIN_COS;
+			let deltaX_Ax = (x2Axis - x1Axis)/NUM_ANIMATION_SIN_COS;
+			let deltaY_Pt = (y2Graph - y1Circ)/NUM_ANIMATION_SIN_COS;
+			let deltaY_Ax = (y2Axis - y1Axis)/NUM_ANIMATION_SIN_COS;
+			for (var i=1; i <= NUM_ANIMATION_SIN_COS; i++) {
+				line = {"begin_x": Math.round(x1Axis + i * deltaX_Ax), 
+						"begin_y": Math.round(y1Axis + i * deltaY_Ax), 
+						"end_x": Math.round(x1Circ + i * deltaX_Pt), 
+						"end_y": Math.round(y1Circ + i * deltaY_Pt) };
+				console.log(" line is begin_x=" + line.begin_x + " begin_y=" + line.begin_y + " end_x=" + line.end_x + " end_y" + line.end_y);
+				sinLineMovement.push(line);
+			}
+		} else if (whichGraph == "cos") {
+			// cos line starts out horizontal and ends up vertical, try doing in one step...
+		}		
+	}
+	
+	// animate the sine/cos lines from circle to move to the graphs
+	let doAnimation = false;
+	let ind = 0;
+	let preAnimatePlot;
+	setInterval(function(){
+		if (doAnimation) {
+			ctxExpandableUnitCircle.putImageData(preAnimatePlot, 0, 0);
+			ctxExpandableUnitCircle.beginPath();
+			ctxExpandableUnitCircle.lineWidth = 3.0;
+			ctxExpandableUnitCircle.strokeStyle = 'blue';
+			ctxExpandableUnitCircle.moveTo(sinLineMovement[ind].begin_x, sinLineMovement[ind].begin_y);
+			ctxExpandableUnitCircle.lineTo(sinLineMovement[ind].end_x, sinLineMovement[ind++].end_y);
+			ctxExpandableUnitCircle.stroke();
+			console.log(" inside animation, ind = " +ind+" total looping is "+sinLineMovement.length);
+			if (sinLineMovement.length == ind) {
+				doAnimation = false;
+				// we are done with this animation array, empty it and reset counter
+				sinLineMovement = [];
+				ind = 0;
+			}
+		}
+	
+	}, 500); // every 500 ms, move the animation
+	
+	
 	function isInside(point, circle, radius) {
 		return ((point.x-circle.x) ** 2 + (point.y - circle.y) ** 2) <= (radius) ** 2;
 	};
-
+	
+	// this code is used as user interacts with the yellow dots on main circle
 	circleDotsCanvas.addEventListener('click', (e) => {
 	
 		// need to convert canvas coord into bitmap coord
@@ -352,7 +481,6 @@ $(function() {
 		schoolAngles.forEach(dot => {
 			// not sure yet which dot the user clicked on, must search all
 			let result = (isInside(pos, dot, DOT_RADIUS));
-			cntr = cntr + 1;
 			if (isInside(pos, dot, DOT_RADIUS)) {
 				// we found the dot the user clicked on
 		  		//clear any old drawings before we put up the new stuff, take it back to the background image
@@ -401,12 +529,18 @@ $(function() {
 				ctxExpandableUnitCircle.font = '20px Arial';
 				ctxExpandableUnitCircle.fillText("\u03b8", CIRC_X0 + ANGLE_IND, CIRC_Y0 - ANGLE_IND/4);	
 				ctxExpandableUnitCircle.stroke();
-				ctxExpandableUnitCircle.closePath();	
+				ctxExpandableUnitCircle.closePath();
+				
+				// snap a picture of what we have so we can go back to it during animation
+
+				preAnimatePlot = ctxExpandableUnitCircle.getImageData(0, 0, circleDotsCanvas.width, circleDotsCanvas.height);	
+				
+				// now animate the sine/cos lines going over to graph, first fill in the array, then turn on animate
+				animate_circ2graph("sin", dot.x, CIRC_Y0, dot.x, dot.y, cntr);
+				doAnimation = true;
 		    }
+		    cntr = cntr + 1;
 		});
 	});
-
-  
-
 	
 })
