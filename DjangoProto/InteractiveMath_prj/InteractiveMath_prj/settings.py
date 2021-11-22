529d90dcfcbdc213c28a323133f970ab48b2a22b
ccdm-edu <cathy.demeyer@yahoo.com>
2021-11-22 18:25:57 -0500

    fixing debug on remote server
    Change-Id: Ic74c53a7be54bce5f48cdee42c34fb7b3625f59e

--- DjangoProto/InteractiveMath_prj/InteractiveMath_prj/settings.py
+++ DjangoProto/InteractiveMath_prj/InteractiveMath_prj/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.environ.get('INT_MATH_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG_str = os.environ.get('DEBUG')
 DEBUG = False
+#
 if DEBUG_str.lower() == 'true':
     DEBUG = True
 
