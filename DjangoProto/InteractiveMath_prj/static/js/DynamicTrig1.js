357aaace82556480a055643847d820a3979b1f38
ccdm-edu <cathy.demeyer@yahoo.com>
2021-12-13 17:10:52 -0500

    working on dynamic phase accum page
    Change-Id: I356525d41aac99ff38f48de8d7ed7f4b02038c4e

--- DjangoProto/InteractiveMath_prj/static/js/DynamicTrig1.js
+++ DjangoProto/InteractiveMath_prj/static/js/DynamicTrig1.js
@@ -32,11 +32,14 @@ $(function() {
     ctxUnitCircle.stroke();
     
     // angle in rad, draw small yellow circles that user can click on to accumulate phase
+    let littleDotCenter = [];
+
     let ANGLE_PER_PT = Math.PI/6;
     for (let pt = 0; pt < 2.0 * Math.PI/ANGLE_PER_PT; pt++) {
     	let curr_angle = pt * ANGLE_PER_PT;
     	let x = CIRC_X0 + Math.round(CIRC_RAD*Math.cos(curr_angle));
 		let y = CIRC_Y0 - Math.round(CIRC_RAD*Math.sin(curr_angle));
+		littleDotCenter.push({x: x, y: y});
     	ctxUnitCircle.beginPath();
 		// create dots for user clickable phase accumulation
 		ctxUnitCircle.arc(x, y, DOT_RADIUS, 0, 2 * Math.PI, true);
@@ -51,27 +54,56 @@ $(function() {
 	let countTime;
 	let timerStarted = false;
 	let startInterval;
+	let totalPhase;
 
     $('#StartPhaseAccum_DT1').on('click', function(event) {
         if (!timerStarted) {
             timerStarted = true;
             countTime =0;
+            totalPhase = 0;
             $(this).prop('disabled', true);
+            $('#UserNotices_DT1').text('');
             startInterval = setInterval(function(){
 	    		countTime++;
 	    		$('#timeVal_DT1').text(roundFP(countTime * 0.1, 1)); 
 	    		if (countTime == 100) {
-	    			console.log('we made 100');
 	    			//After 10 sec, end the experiment
 	        		if (startInterval) clearInterval(startInterval);
-	        		$(this).prop('disabled', false);
+	        		$('#StartPhaseAccum_DT1').prop('disabled', false);
 	        		timerStarted = false;
+	        		$('#UserNotices_DT1').text('Time expired for accumulating 360 degrees of phase.  Try again');
+	        	}
+	        	if (totalPhase >= 360) {
+	        		if (startInterval) clearInterval(startInterval);
+	        		timerStarted = false;
+	        		$('#UserNotices_DT1').text('');
+	        		// do everything else first
+	        		$('#StartPhaseAccum_DT1').prop('disabled', false);
 	        	}
 	    	}           
             , 100);
         }
 
     });
+    console.log(littleDotCenter);
+    circleDotsCanvas.addEventListener('click', (e) => {
+    	// need to convert canvas coord into bitmap coord
+		var rect = circleDotsCanvas.getBoundingClientRect();
+		const pos = {
+		  x: e.clientX - rect.left,
+		  y: e.clientY - rect.top
+		};	
+		console.log(' clicked at point (' + pos.x + ',' + pos.y + ')');
+		let ind = 0;
+		littleDotCenter.forEach(dot => {
+			// not sure yet which dot the user clicked on, must search all\
+			ind = ind + 1;
+			console.log('checking the ' + ind + ' point');
+			if (isInside(pos, dot, DOT_RADIUS)) {
+				console.log(' you clicked the ' + ind + ' point');
+			}
+		});
+	});
     
 	// User can choose a TO DO set for the text box or an explanation, this code is the implementation
 	$('#ToDo_or_expln_DT1').on('click', function(event){
