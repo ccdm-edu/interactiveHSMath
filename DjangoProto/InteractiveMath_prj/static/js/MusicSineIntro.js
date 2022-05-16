b3c632bc5d16cab5bba1011603f010cb264de6db
ccdm-edu <cathy.demeyer@yahoo.com>
2022-05-16 16:09:31 -0400

    starting music intro page notes
    Change-Id: Idd9cd46f6d2efe5e5351e728acc1f9df646899a0

---
+++ DjangoProto/InteractiveMath_prj/static/js/MusicSineIntro.js
@@ -0,0 +1,29 @@
+'use strict'
+//JQuery, dont do this script until document DOM objects are loaded and ready
+$(function() {
+	let musicCanvas =  $("#ClefWithNotes").get(0);  // later on, this will set the "background image" for animation
+	// get ready to start drawing on this canvas, first get the context
+	let ctxMusicCanvas;
+	if ( $("#ClefWithNotes").length ) {
+    	ctxMusicCanvas = $("#ClefWithNotes").get(0).getContext('2d');
+	} else {
+    	console.log('Cannot obtain Trumpet b flat notes context');
+	}
+	const LEFT_X = 20;
+	const RIGHT_X = 1000; 
+	const START_Y = 20;
+	const DELTA_Y = 40; 
+	for (let ind = 0; ind<5; ind++) {
+		ctxMusicCanvas.beginPath();
+		ctxMusicCanvas.moveTo(LEFT_X, START_Y + ind*DELTA_Y);
+		ctxMusicCanvas.lineTo(RIGHT_X, START_Y + ind*DELTA_Y);
+		ctxMusicCanvas.stroke();
+		ctxMusicCanvas.closePath();
+	}
+	//Add small line at bottom of low C
+	ctxMusicCanvas.beginPath();
+	ctxMusicCanvas.moveTo(LEFT_X + 150, START_Y + 5 * DELTA_Y);
+	ctxMusicCanvas.lineTo(LEFT_X + 220, START_Y + 5 * DELTA_Y);
+	ctxMusicCanvas.stroke();
+	ctxMusicCanvas.closePath();	
+})
