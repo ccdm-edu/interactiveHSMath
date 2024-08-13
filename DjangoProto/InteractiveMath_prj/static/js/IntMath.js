b2c5ca6a532b965a8d5cb0d7b5d15934bdb65f84
ccdm-edu <cathy.demeyer@yahoo.com>
2024-08-13 13:20:04 -0400

    fixed problem of alignment for bottom footer

--- DjangoProto/InteractiveMath_prj/static/js/IntMath.js
+++ DjangoProto/InteractiveMath_prj/static/js/IntMath.js
@@ -140,10 +140,13 @@ $(function () {
 		( navigator.msMaxTouchPoints > 0 );
 	};
 	if (is_touch_enabled()) {
+		// the jquery.touch.min.js solution is suboptimal but at least we can move the window for touch devices--need to tell user
+		// they now have to double "click" to close
 		$("#advModal").attr("data-original-title", "Select and touch to move or mouse drag.  Double touch to close");
 		$("#advModalClose").attr("data-original-title", "Double touch to close")
 	} else {
-		// using mouse, jquery ui draggable will work fine
+		// using mouse, jquery ui draggable will work ok.  Have to grab it on top label to move it
+		// a bit flaky with jquery.touch.min.js in there
 		$("#advModal").attr("data-original-title", "drag me around as needed");
 	}
 
